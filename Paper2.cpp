b4e12e007bd78e2098f3933801ce120c10d006aa
Inti Ruiz <ilruizc@unal.edu.co>
2021-12-30 00:46:44 -0500

    Implementación de la regla de evolución completada. Probada para arreglos pequeños, con resultados dentro de lo esperado. Para arreglos grandes, el tiempo de computo excede los 30 min

--- Paper2.cpp
+++ Paper2.cpp
@@ -3,18 +3,21 @@
 #include <fstream>
 #include <random>
 #include <math.h>
+#include <time.h>
 
-const short int N = 20; //Tamaño del arreglo
+const short int N = 200; //Tamaño del arreglo
 const short int T = 200; // Tiempo máximo
-const short int Q = 9; //Número de estados
-const short int r = 6; //Radio en celdas de la región caliente
-const float E_A = 2; // Energía de activación, como multiplo de la constante de Boltzmann 
+const short int Q = 100; //Número de estados
+const short int r = 5; //Radio en celdas de la región caliente
+const float E_A = 2; // Energía de activación, como multiplo de la constante de Boltzmann
+const float R = 8.31446261; // Constante del Gas ideal
 
 class Material
 {
 private:
   short int h_old[N][N];
   short int h_new[N][N];
+  int Delta_E_B [25];
   float Delta_E[N][N];
   short int neighborhood [25]; //Numero de segundos vecinos de Moore
   float tempt [N][N];
@@ -26,6 +29,10 @@ public:
   float probability (int i, int j, float M_max, float D_Emin);
   void neighbor_def (int i, int j);
   int neighbor_get (int ii);
+  bool is_boundary (int i, int j);
+  long int Boundary_energy (void);
+  void Delta_E_min (int i, int j);
+  void evolve (void);
   // void size_count (void);
   void print_array(const char * Arreglo);
   void print_gradient(const char * Arreglo);
@@ -51,12 +58,11 @@ void Material::fill (void){
 	h_old[i][j] = h_old[i][0];
       }
       else {
-	  h_old[i][j]= rand(gen1);
-	
+	h_old[i][j]= rand(gen1);
+	h_new[i][j]=rand(gen1);
       }
     }
   }
-  h_old[N/2][N/2] = 0;
 }
 void Material::fill_tempt(float T_min, float T_max){
   float a = r*(T_max-T_min);
@@ -531,7 +537,9 @@ void Material::neighbor_def (int i, int j){
        }
      }
    }
+   
    else{
+     n_index=0;
      for(int ii = i-2; ii <= i+2; ii++){
        for(int jj = j-2; jj <= j+2; jj++){
 	 neighborhood[n_index] = h_old[ii][jj];
@@ -539,6 +547,161 @@ void Material::neighbor_def (int i, int j){
        }
      }
    }
+}
+
+bool Material::is_boundary (int i, int j){
+  int actual = h_old [i][j];
+  int test = 0;
+  int i_aux =  0;
+  int j_aux = 0;
+  neighbor_def (i,j);
+  for (int ii = 0; ii < 3; ii++){
+    for (int jj = 0; jj<3; jj++){
+      test = neighborhood [6+(3*i_aux)+j_aux];
+      if (test != actual){
+	return true;
+      }
+      j_aux++;
+    }
+    j_aux--;
+    i_aux++; 
+  }
+  return false;
+}
+
+long int Material::Boundary_energy (void){
+ long int Energy = 0;
+  for (int ii=0;ii<N;ii++){
+    for(int jj=0;jj<N;jj++){
+      for(int u=0;u<25;u++){
+	if (h_old[ii][jj]!=neighborhood[u]){
+	  Energy++;
+	  //  std::cout<<Energy<<std::endl;
+	} 
+      }
+    }
+  }
+  return Energy;
+}
+
+void Material::Delta_E_min (int i, int j){
+  
+  long int EB_0 = 0;
+  long int EB_f = 0;
+  int EB = 0;
+  int E_aux = 0;
+  int value_aux = h_old[i][j];
+  int count = 0;
+  int rand_part = 0;
+  float Delta_ET = 0.0;
+  
+
+  std::mt19937 gen(time(NULL));
+  std::uniform_real_distribution<> dis(0, 1);
+  
+  Delta_ET = -R*tempt [i][j]*log (dis(gen));
+  EB_0 = Boundary_energy ();
+  
+  for (int u = 0; u < 25 ; u++){
+    if(u==12){
+      neighborhood [12] = value_aux;
+      h_old [i][j] = value_aux;
+    }
+    else{
+      neighborhood [12] = neighborhood [u];
+      h_old [i][j] = neighborhood [u];
+    }
+    EB_f = Boundary_energy ();
+    EB = EB_f-EB_0;
+    if (EB<E_aux){
+      E_aux=EB;
+      h_new[i][j] = h_old[i][j];
+    }
+    Delta_E_B [u] = EB;
+  }
+  
+  for(int u = 0; u<25; u++){
+    if(Delta_E_B[u] == E_aux){
+      count++;
+      }
+  }
+  // 
+  if (count > 1){
+    float part = 100.0/count;
+    float rand_num = dis(gen) *100.0;
+    rand_part = round(rand_num/part);
+    
+    if (rand_part==0){
+      rand_part++;
+    }
+    count = 0;
+    for(int u = 0; u<25; u++){
+      if(Delta_E_B[u] == E_aux){
+	count++;
+	if(count == rand_part){
+	  
+	  h_new [i][j] = neighborhood [u];
+	  break;
+	}
+      }  
+    }
+  }
+  Delta_E [i][j] = EB-Delta_ET;
+  h_old[i][j] = value_aux;
+}
+
+void Material::evolve (void){
+  float D_Emin = 0.0;
+  float M_max = exp ((-E_A)/tempt [N/2][N/2]);
+
+  std::mt19937 gen(time(NULL));
+  
+  
+  for (int i=0; i<N; i++){
+    for(int j=0; j<N; j++){
+      if (is_boundary(i,j)==true){
+	Delta_E_min(i,j);
+      }
+      else {
+	Delta_E [i][j] = 0;
+      }
+    }
+  }
+  D_Emin = Delta_E [0][0];
+  for (int i=0; i<N; i++){
+    for(int j=0; j<N; j++){  
+      if(Delta_E [i][j] < D_Emin){
+	D_Emin = Delta_E[i][j];
+      }
+    }
+  }
+  for (int i=0; i<N; i++){
+    for(int j=0; j<N; j++){
+      if (Delta_E [i][j] != 0){
+	std::bernoulli_distribution prob(/*probability (i, j, M_max, D_Emin)*/1);
+	//std::cout<<probability (i, j, M_max, D_Emin)<<std::endl;
+	if (prob(gen) == false){
+	  //std::cout<<"aqui"<<std::endl;
+	  h_new[i][j] = h_old [i][j];
+	}
+      }
+    }
+  }
+
+  for (int i=0; i<N; i++){
+    for(int j=0; j<N; j++){
+      if (j == N-1){
+	h_old [i][j] = h_new[i][0];
+      }
+      else if (i== N-1){
+	h_old [i][j] = h_new [0][j];
+      }
+      else {
+	h_old [i][j] = h_new [i][j];
+      }
+    }
+  }
+  
 }
 
 int Material::neighbor_get (int ii){
@@ -564,22 +727,16 @@ void Material::print_gradient(const char * Arreglo)
   MiArchivo.close();
 }
 int main (void){
-   Material granos;
-   granos.fill_tempt(800.0,1000.0);
-   granos.fill();
-   granos.print_array("Arreglo1.txt");
-   granos.neighbor_def(N/2,N-1);
-   int neighbor [25];
-   for (int ii = 0; ii<25 ; ii++){
-     neighbor[ii] = granos.neighbor_get (ii);
-   }
-   for(int i=0;i<5;i++){
-    for(int j=0;j<5;j++){
-      std::cout<<neighbor[(5*i)+j]<<'\t';
-    }
-    std::cout<<std::endl;
-   }
-   
+  int a = 0;
+  Material granos;
+  granos.fill_tempt(1000.0,1000.0);
+  granos.fill();
+  granos.print_array("Arreglo1.dat");
+  for (int t = 0; t <2; t++){
+    granos.evolve();
+  }
+  granos.print_array("Arreglo2.dat");
+  
   return 0;
   
 }
